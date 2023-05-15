bdad4a705a2d4aa07081ac4470459b8782747b7a
Rohan saini <rohansaini913@gamil.com>
2023-05-15 16:58:13 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
-
 public class Main {
-
+    static class Product{
+        public int product(int x, int y) {
+            int i= x+y;
+            return i;
+        }
+        public int product(int x, int y, int z) {
+            int i=x+y+z;
+            return i;
+        }
+        public double product(double x, double y) {
+            Double d=x+y;
+            return d;
+        }
+    }
+    public static void main(String args[]){
+        Product p=new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(10,40);
+    }
 }
